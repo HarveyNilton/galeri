fc82fdcbda105bcdad9a1532c4b6760531acebd7
HarveyNilton <65696942+HarveyNilton@users.noreply.github.com>
2023-04-19 16:46:41 -0500

    Add styled

--- src/compoinent/Galeria1.jsx
+++ src/compoinent/Galeria1.jsx
@@ -1,18 +1,43 @@
-const Flowers = () =>{
-   
+import styled from "styled-components";
 
-    return (
-      <div className="App">
-        <div className="galeri">
-        <section>
-            <img src="https://storage.googleapis.com/fpc_fotos_cdn/fotos2/92-72/1_w800_q80.webp" alt="" />
-            <img src="https://storage.googleapis.com/fpc_fotos_cdn/fotos2/550/1_w800_q80.webp" alt="" />
-            <img src="https://storage.googleapis.com/fpc_fotos_cdn/fotos2/390/1_w800_q80.webp" alt="" />
-            <img src="https://storage.googleapis.com/fpc_fotos_cdn/fotos2/127-86/1_w800_q80.webp" alt="" />
-        </section>
-        </div> 
+const Flowers = () => {
+
+
+  return (
+    <div className="App">
+      <div className="galeri">
+        <Section>
+          <ContentImg src="https://storage.googleapis.com/fpc_fotos_cdn/fotos2/92-72/1_w800_q80.webp" alt=""></ContentImg>
+          <ContentImg src="https://storage.googleapis.com/fpc_fotos_cdn/fotos2/550/1_w800_q80.webp" alt=""></ContentImg>
+          <ContentImg src="https://storage.googleapis.com/fpc_fotos_cdn/fotos2/390/1_w800_q80.webp" alt=""></ContentImg>
+          <ContentImg src="https://storage.googleapis.com/fpc_fotos_cdn/fotos2/127-86/1_w800_q80.webp" alt="" ></ContentImg>
+        </Section>
+
       </div>
-    )
-  }
-  
-  export default Flowers;
+    </div>
+  )
+}
+
+export default Flowers;
+
+const Section = styled.section`
+  display: flex;
+  width: 600px;
+  height: 430px;
+  gap: 1rem;
+
+`
+const ContentImg = styled.img`
+    width: 0px;
+    flex-grow: 1;
+    object-fit: cover;
+    opacity: .8;
+    transition: 3s ease;
+
+    &:hover {
+      cursor: crosshair;
+      width: 300px;
+      opacity: 1;
+      filter: contrast(120%);
+    }
+`
--- src/compoinent/Img3D.jsx
+++ src/compoinent/Img3D.jsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 const Mario = () =>{
 
     return(
@@ -10,3 +12,50 @@ const Mario = () =>{
     )
 }
 export default Mario;
+
+const Article = styled.article`
+  display: flex;
+  width: 600px;
+  height: 430px;
+  position: relative;
+  transition: all .3s ease;
+
+  &:hover {
+    transform: perspective(250px)
+    rotateX(10deg)
+    translateY(-5%)
+    translateZ(0);
+  }
+  
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    height: 100%;
+    width: 100%;
+    background-image: linear-gradient(
+      to bottom,
+      transparent 10%,
+      rgba (0,0,0,0.5) 50%,
+      rgba (0,0,0) 95%
+    );
+    opacity: 0;
+    transition: all .3s ease;
+  }
+
+
+  > img{
+
+    &:last-child {
+        position: absolute;
+        width: 550px;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+        transform: translateY(25%);
+        transition: .3s ease;
+        opacity: 0;
+    }
+  }
+`
