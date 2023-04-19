93950f3ef734203a81bd77958ad986465390b796
Ma.Fer <myfirstvape.2019@gmail.com>
2023-04-19 15:46:21 -0500

    Add img 3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Flowers from './compoinent/Galeria1'
+import Mario from './compoinent/Img3D'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Flowers/>
+      <Mario/>
     </div>
   )
 }
---
+++ src/compoinent/Img3D.jsx
@@ -0,0 +1,12 @@
+const Mario = () =>{
+
+    return(
+        <div className="3d" > 
+            <article>
+                <img src="https://as.com/meristation/imagenes/2013/10/08/album/1381235520_235520_000001_album_normal.jpg" alt="" />
+                <img src="https://fs-prod-cdn.nintendo-europe.com/media/images/08_content_images/games_6/nintendo_switch_7/nswitch_supermario3dlandandbowsersfury/SuperMario3DWorld_BowersFury_Overview_intro_chars.png" alt="" />
+            </article>
+        </div>
+    )
+}
+export default Mario;
